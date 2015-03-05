f0f6a4ccf742d462d2066cfeca2a211a1505ce41
Defectuous <defectuous@mac.com>
2015-03-05 00:02:08 -0700

    Update FourNotes.cs
    Revision: 1.1.0.0
    Used example code from " [Plugin] [Example] Autofollower/Healer Base " 
    Revised it to meet the needs of this plugin.

--- FourNotes.cs
+++ FourNotes.cs
@@ -6,60 +6,93 @@ using System.Collections.Generic;
 using System.Linq;
 using ArcheBuddy.Bot.Classes;
 
-namespace DefaultNameSpace{
+//
+// Special Thanks to 2face for the following thread " [Plugin] [Example] Autofollower/Healer Base "  
+// 
+
+
+namespace ArcheAgeFourNotes{
     public class FourNotes : Core
    {
        public static string GetPluginAuthor()
        { return "Defectuous"; }
        public static string GetPluginVersion()
-       { return "1.0.0.7"; }
+       { return "1.1.0.0"; }
        public static string GetPluginDescription()
-       { return "Four Notes: Part/Raid Songcraft Buffs Plugin"; }
-        
-       //Call on plugin start
+       { return "Four Notes: Songcraft four songs"; }
+       
+       private Creature _leader = null;
+       private Double _bardRange = 19.5;
+       private Double _followRange = 10.0;
+       
        public void PluginRun()
-       {
-                while (me.target == null)
-                {
-                    Thread.Sleep(50);
+        {    
+            _leader = getPartyLeaderObj();
+            if(_leader == null || _leader == me){
+                Log("Please set leader");
+            }else{
+                Start();
+            }
+            
+        }  
+        
+        public void Start(){
+            while(true){
+                Thread.Sleep(50);
+                if(_leader != null){
+                    if(_leader.inFight && me.dist(_leader) < _bardRange){
+                        bardRoutine();
+                    }else{
+                        moveToPlayer(_leader);  
+                    }
+                    
                 }
                 
-                while (me.target != null)
-                {
-                    Log("----------------------------------------------");
-                    Log("Starting Rotation");
-                    if (skillCooldown("[Perform] Bulwark Ballad") == 0 )
-                    {
-                        UseSkill("[Perform] Bulwark Ballad");
-                        Log("Casting Bulwark Ballad");
-                        Thread.Sleep(2075);
-                    }
+            }
+        }
             
-                    if (skillCooldown("[Perform] Bloody Chantey") == 0 )
-                    {
-                        UseSkill("[Perform] Bloody Chantey");
-                        Log("Casting Bloody Chantey");
-                        Thread.Sleep(2075);
-                    }
+         public void moveToPlayer(Creature obj){
+            ComeTo(obj, _followRange);
+        }
+        
+        public void bardRoutine()
+        {
+            Log("----------------------------------------------");
+            Log("Starting Rotation");
+            if (skillCooldown("[Perform] Bulwark Ballad") == 0 )
+                {
+                    UseSkill("[Perform] Bulwark Ballad");
+                    Log("Casting Bulwark Ballad");
+                    Thread.Sleep(2075);
+                }
+                    
+            if (skillCooldown("[Perform] Bloody Chantey") == 0 )
+                {
+                    UseSkill("[Perform] Bloody Chantey");
+                    Log("Casting Bloody Chantey");
+                    Thread.Sleep(2075);
+                }
+                    
+            if (skillCooldown("[Perform] Ode to Revery") == 0 )
+                {
+                    UseSkill("[Perform] Ode to Revery");
+                    Log("Casting Ode to Revery");
+                    Thread.Sleep(2075);
+                }
                 
-                    if (skillCooldown("[Perform] Ode to Recovery") == 0 )
-                    {
-                        UseSkill("[Perform] Ode to Recovery");
-                        Log("Casting Ode to Recovery");
-                        Thread.Sleep(2075);
-                    }
-                
-                    if (skillCooldown("[Perform] Quickstep") == 0 )
-                    {
-                        UseSkill("[Perform] Quickstep");
-                        Log("Casting Quickstep");
-                        Thread.Sleep(2075);
-                    }
-                    Thread.Sleep(3000);
-                    Log("Sleeping for 3 Seconds");
-                } 
-       }
-       //Call on plugin stop
-       public void PluginStop()
-       {
-       }
+            if (skillCooldown("[Perform] Quickstep") == 0 )
+                {
+                    UseSkill("[Perform] Quickstep");
+                    Log("Casting Quickstep");
+                    Thread.Sleep(2075);
+                }
+            Thread.Sleep(3000);
+            Log("Sleeping for 3 Seconds");
+        }
+
+        //Call on plugin stop
+        public void PluginStop()
+        {
+        }
+    }
+}
