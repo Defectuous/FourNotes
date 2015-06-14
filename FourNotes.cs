e3b4373bb99fe38143a0146f2e6d9e9debd0f96a
Defectuous <defectuous@mac.com>
2015-06-13 21:53:00 -0600

    Update FourNotes.cs
    Revision 1.0.1.19
    Updated so is checks if a spell is available for casting.
    Editing following thread sleeps.

--- FourNotes.cs
+++ FourNotes.cs
@@ -13,34 +13,33 @@ namespace ArcheAgeFourNotes
         public static string GetPluginAuthor()
         { return "Defectuous"; }
         public static string GetPluginVersion()
-        { return "1.0.1.17"; }
+        { return "1.0.1.19"; }
         public static string GetPluginDescription()
         { return "4 Notes: Party/Raid Songcraft Buffs Plugin"; }
 
         
         private bool _followMode = true;
-        private Double _followRange = 10.0;
-                // Do not EDIT Below this line 
-        // ##########################################################
+        private Double _followRange = 5.0;
         private Creature _leader = null;
         
+        // Do not EDIT Below this line 
+        // ##########################################################
         //Call on plugin start
         public void PluginRun()
         {
             ClearLogs();
-        
-            // Starting Threads
             Log(Time() + "[INFO]: STARTING 4 NOTES");
             _leader = getPartyLeaderObj();    
-            
+
+            // Starting Threads
             Thread followThread = new Thread(new ThreadStart(FollowTheLeader));
             
             if (_leader == null || _leader == me)
             { Log(Time() + "[WARNING]: Please set a Leader other than yourself or join a Party/Raid"); } 
                 else  { 
-                    if (_followMode == true){ followThread.Start(); }
+                    if (_followMode == true){ followThread.Start(); Log(Time() + "[INFO]: Starting Following Thread"); }
                     Songs();
-                    if (_followMode == true){ followThread.Abort(); }
+                    if (_followMode == true){ followThread.Abort(); Log(Time() + "[INFO]: Ending Following Thread"); }
                 }
             Log(Time() + "[INFO]: ENDING 4 NOTES");
             
@@ -50,8 +49,8 @@ namespace ArcheAgeFourNotes
         public void FollowTheLeader()
         {
             while(true) {
-                Log(Time() + me.dist(_leader));
-                    
+                Log(Time() + "Distance to Leader:" +me.dist(_leader));
+                Thread.Sleep(3500);     
                 if (me.dist(_leader) >= _followRange)
                 { moveToPlayer(_leader); Thread.Sleep(3500); }
             } 
@@ -70,36 +69,33 @@ namespace ArcheAgeFourNotes
             while (true)
             {
 
-                if (skillCooldown("[Perform] Bulwark Ballad") == 0)
+                if (isSkillLearned("[Perform] Bulwark Ballad") && skillCooldown("[Perform] Bulwark Ballad") == 0)
                 {
                     UseSkill("[Perform] Bulwark Ballad");
                     Log(Time() + "[INFO]: Casting Bulwark Ballad");
-                    Thread.Sleep(2000);
-                }
+                } Thread.Sleep(2000);
 
-                if (skillCooldown("[Perform] Bloody Chantey") == 0)
+                if (isSkillLearned("[Perform] Bloody Chantey") && skillCooldown("[Perform] Bloody Chantey") == 0)
                 {
                     UseSkill("[Perform] Bloody Chantey");
                     Log(Time() + "[INFO]: Casting Bloody Chantey");
-                    Thread.Sleep(2000);
-                }
+                } Thread.Sleep(2000);
 
-                if (skillCooldown("[Perform] Ode to Recovery") == 0)
+                if (isSkillLearned("[Perform] Ode to Recovery") && skillCooldown("[Perform] Ode to Recovery") == 0)
                 {
                     UseSkill("[Perform] Ode to Recovery");
                     Log(Time() + "[INFO]: Casting Ode to Recovery");
-                    Thread.Sleep(2000);
-                }
+                } Thread.Sleep(2000);
 
-                if (skillCooldown("[Perform] Quickstep") == 0)
+                if (isSkillLearned("[Perform] Quickstep") && skillCooldown("[Perform] Quickstep") == 0)
                 {
                     UseSkill("[Perform] Quickstep");
                     Log(Time() + "[INFO]: Casting Quickstep");
-                    Thread.Sleep(2000);
-                }
+                } Thread.Sleep(2000);
+                
                 Log(Time() + "[INFO]: Sleeping for 22 Seconds");
                 Thread.Sleep(21500);
-                Log(Time() + "[INFO]: Next Rotation");
+                Log(Time() + "[INFO]: Starting Next Rotation");
                 
             }
         }
